562c00ceadb3b9d487020561c6cd134389842d3b
Davide Pedranz <davide.pedranz@gmail.com>
2015-12-14 23:10:16 +0100

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: 'email@email.com', password:'password')
+# create one default user
+User.create(email: 'email@email.com', password: 'password')
+
+# create some articles
+Article.create(title: 'Lorem ipusm', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur,
+  ea est. Ducimus molestiae nam sint. Accusantium asperiores aut, eos et excepturi laboriosam laudantium,
+  mollitia nam odit officiis perferendis quia velit?')
+Article.create(title: 'A aspernatur', text: 'A aspernatur beatae blanditiis, cumque dignissimos est eveniet iusto
+  nam nobis numquam odit ullam ut velit veniam voluptates. Assumenda consectetur dolore hic, iusto laboriosam magni
+  nisi numquam repellat saepe sunt.')
